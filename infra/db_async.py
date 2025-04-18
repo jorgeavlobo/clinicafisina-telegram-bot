328efda5d3bc839910a059dc8d32458ca1685878
Jorge Lobo <jorgeavlobo@gmail.com>
2025-04-18 01:16:01 +0100

    Update db_async.py

--- infra/db_async.py
+++ infra/db_async.py
@@ -10,21 +10,23 @@ DB_PORT = int(os.getenv("DB_PORT", 5432))
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME_FISINA = os.getenv("DB_NAME_FISINA", "fisina")
-DB_NAME_LOGS = os.getenv("DB_NAME_LOGS", "logs")
+DB_NAME_LOGS   = os.getenv("DB_NAME_LOGS", "logs")
 
 class DBPools:
     pool_fisina: asyncpg.Pool | None = None
-    pool_logs: asyncpg.Pool | None = None
+    pool_logs:   asyncpg.Pool | None = None
 
     @classmethod
     async def init(cls):
         cls.pool_fisina = await asyncpg.create_pool(
             host=DB_HOST, port=DB_PORT, user=DB_USER,
-            password=DB_PASSWORD, database=DB_NAME_FISINA, min_size=1, max_size=5
+            password=DB_PASSWORD, database=DB_NAME_FISINA,
+            min_size=1, max_size=5
         )
         cls.pool_logs = await asyncpg.create_pool(
             host=DB_HOST, port=DB_PORT, user=DB_USER,
-            password=DB_PASSWORD, database=DB_NAME_LOGS, min_size=1, max_size=5
+            password=DB_PASSWORD, database=DB_NAME_LOGS,
+            min_size=1, max_size=5
         )
 
     @classmethod
