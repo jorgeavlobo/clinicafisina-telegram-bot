293085b468e6e8c67eb6fb68f138b4d5f9cc55cc
Jorge Lobo <jorgeavlobo@gmail.com>
2025-04-22 14:57:28 +0100

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,12 @@
+from importlib import import_module
+from pathlib import Path
 
+# Dynamically import all routers for auto‑registration
+all_routers = []
+
+for path in Path(__file__).parent.rglob("*_router.py"):
+    module_path = ".".join(path.relative_to(Path(__file__).parent).with_suffix("").parts)
+    mod = import_module(f"handlers.{module_path}")
+    router = getattr(mod, "router", None)
+    if router:
+        all_routers.append(router)
