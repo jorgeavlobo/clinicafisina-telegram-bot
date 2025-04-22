38b659ad75fa593805b31f3e5e6d464d08d59db8
Jorge Lobo <jorgeavlobo@gmail.com>
2025-04-22 15:12:30 +0100

    Update redis_fsm.py

--- shared/redis_fsm.py
+++ shared/redis_fsm.py
@@ -1,1 +1,10 @@
+from redis.asyncio import Redis
+from shared.config import settings
 
+def build_redis() -> Redis:
+    return Redis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        db=settings.REDIS_DB,
+        decode_responses=False,
+    )
