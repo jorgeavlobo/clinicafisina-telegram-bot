f9bade4131e7a92990ce7d9246bc8e2d50938c15
Jorge Lobo <jorgeavlobo@gmail.com>
2025-04-22 11:26:58 +0100

    Update __init__.py

--- handlers/registration/__init__.py
+++ handlers/registration/__init__.py
@@ -1,4 +1,4 @@
-# torna fácil importar todos de uma vez no main_menu
+# handlers/registration/__init__.py
 from .regist_menu_router import router as regist_menu_router
 from .regist_patient_router import router as regist_patient_router
 from .regist_caregiver_router import router as regist_caregiver_router
