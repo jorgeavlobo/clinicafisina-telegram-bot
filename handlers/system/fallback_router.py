a7e91ad04b70635c0ca43a75fc3b741536d4ea2a
Jorge Lobo <jorgeavlobo@gmail.com>
2025-04-22 16:22:48 +0100

    Delete handlers/system directory

--- handlers/system/fallback_router.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router, types
-
-router = Router(name="system_fallback")
-
-@router.message()
-async def default(msg: types.Message):
-    await msg.answer("❓ Não entendi. Usa o menu ou /start para começar.")
