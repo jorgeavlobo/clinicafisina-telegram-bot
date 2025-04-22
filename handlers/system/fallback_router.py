c0a0ab64135be5a6ef4046f5a024f6dc851077be
Jorge Lobo <jorgeavlobo@gmail.com>
2025-04-22 15:09:27 +0100

    Update fallback_router.py

--- handlers/system/fallback_router.py
+++ handlers/system/fallback_router.py
@@ -1,1 +1,7 @@
+from aiogram import Router, types
 
+router = Router(name="system_fallback")
+
+@router.message()
+async def default(msg: types.Message):
+    await msg.answer("❓ Não entendi. Usa o menu ou /start para começar.")
