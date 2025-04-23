c93a9de1f37c1379c08709e35e5682eb2ffb0991
Jorge Lobo <jorgeavlobo@gmail.com>
2025-04-23 17:35:42 +0100

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,15 +1,15 @@
-from .auth import router as auth_router
 from .accountant_handlers import router as accountant_router
-from .patient_handlers import router as patient_router
-from .caregiver_handlers import router as caregiver_router
-from .physiotherapist_handlers import router as physiotherapist_router
 from .administrator_handlers import router as administrator_router
+from .auth_handlers import router as auth_router
+from .caregiver_handlers import router as caregiver_router
+from .patient_handlers import router as patient_router
+from .physiotherapist_handlers import router as physiotherapist_router
 
 all_routers = (
-    auth_router,
     accountant_router,
-    patient_router,
-    caregiver_router,
-    physiotherapist_router,
     administrator_router,
+    auth_router,
+    caregiver_router,
+    patient_router,
+    physiotherapist_router,
 )
