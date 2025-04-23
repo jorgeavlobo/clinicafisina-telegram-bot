4fd9764ea54eafc0d3cba759acac2894ee8cf847
Jorge Lobo <jorgeavlobo@gmail.com>
2025-04-23 18:02:36 +0100

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,8 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
+BOT_TOKEN   = os.environ["BOT_TOKEN"]
+DB_URL      = os.environ["DATABASE_URL"]
+LOG_LEVEL   = os.getenv("LOG_LEVEL", "INFO")
+TIMEZONE    = os.getenv("LOCAL_TIMEZONE", "Europe/Zurich")
