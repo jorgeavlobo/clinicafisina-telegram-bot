97fedcbddc6696263d9f98cb4b63fc802e67b817
Jorge Lobo <jorgeavlobo@gmail.com>
2025-04-25 00:44:53 +0100

    Update __init__.py

--- bot/states/__init__.py
+++ bot/states/__init__.py
@@ -1,1 +1,10 @@
+# bot/states/__init__.py
+"""
+Re-exporta todos os grupos de estados para facilitar imports:
+    from bot.states import AuthStates, MenuStates
+"""
 
+from .auth_states import AuthStates
+from .menu_states import MenuStates
+
+__all__ = ["AuthStates", "MenuStates"]
