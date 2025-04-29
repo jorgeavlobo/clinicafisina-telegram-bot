4f02d07e1b8d5276b6b56bf0272c351480d7b94f
Jorge Lobo <jorgeavlobo@gmail.com>
2025-04-30 00:14:48 +0100

    Update administrator_menu.py

--- bot/menus/administrator_menu.py
+++ bot/menus/administrator_menu.py
@@ -4,7 +4,20 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def build_menu() -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text="🗓 Agenda",       callback_data="admin:agenda")],
+            [InlineKeyboardButton(text="🗓 Agenda", callback_data="admin:agenda")],
             [InlineKeyboardButton(text="👥 Utilizadores", callback_data="admin:users")],
         ]
     )
+
+def build_user_type_kbd() -> InlineKeyboardMarkup:
+    """
+    Build an inline keyboard for selecting the type of user to add.
+    """
+    keyboard = [
+        [InlineKeyboardButton(text="👤 Cliente", callback_data="role:patient")],
+        [InlineKeyboardButton(text="🤝 Cuidador", callback_data="role:caregiver")],
+        [InlineKeyboardButton(text="🩺 Fisioterapeuta", callback_data="role:physiotherapist")],
+        [InlineKeyboardButton(text="💼 Contabilista", callback_data="role:accountant")],
+        [InlineKeyboardButton(text="💻 Administrador", callback_data="role:administrator")],
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
